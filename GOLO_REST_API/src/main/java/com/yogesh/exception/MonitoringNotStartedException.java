3e8e7640f9b864318ba70d688f446106b60c5427
yogesh thakare <yogtha88@gmail.com>
2017-09-10 18:31:11 -0400

    calling super.

--- GOLO_REST_API/src/main/java/com/yogesh/exception/MonitoringNotStartedException.java
+++ GOLO_REST_API/src/main/java/com/yogesh/exception/MonitoringNotStartedException.java
@@ -4,6 +4,13 @@ package com.yogesh.exception;
  * @author Yogesh Thakare
  */
 public class MonitoringNotStartedException extends Exception {
-	public MonitoringNotStartedException(String message) {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public MonitoringNotStartedException(String message) 
+	{
+		super(message);
 	}
 }

18cbe7fb35a5cb818fdb6f460c5468f1550feb96
yogesh thakare <yogtha88@gmail.com>
2017-09-10 20:29:59 -0400

    new custom exception

--- GOLO_REST_API/src/main/java/com/yogesh/exception/InvalidCommadException.java
+++ GOLO_REST_API/src/main/java/com/yogesh/exception/InvalidCommadException.java
@@ -3,11 +3,11 @@ package com.yogesh.exception;
 /**
  * @author Yogesh Thakare
  */
-public class MonitoringNotStartedException extends Exception {
-	
+public class InvalidCommadException extends Exception {
+
 	private static final long serialVersionUID = 1L;
 
-	public MonitoringNotStartedException(String message) 
+	public InvalidCommadException(String message) 
 	{
 		super(message);
 	}
